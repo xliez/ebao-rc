cfd33d851394b2473e31292425639c8e5d3873c4
xliez <xliez@foxmail.com>
2024-02-19 11:38:00 +0800

    ci: deploy
    Signed-off-by: xliez <xliez@foxmail.com>

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,9 +4,13 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'ebao-rc',
-    nav: [{
-      title: 'components',
-      link: '/components',
-    }]
+    nav: [
+      {
+        title: 'components',
+        link: '/components',
+      },
+    ],
   },
+  base: '/ebao-rc/',
+  publicPath: '/ebao-rc/',
 });
